a41c68b4fc93799bcbcd48c082321984bd8d2556
wiktorsadowski <wiktor.sadowski@landingi.com>
2021-08-23 13:42:10 +0200

    Add delivery controller.

---
+++ src/UI/Rest/Controller/Delivery/DeliveryCommandController.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\UI\Rest\Controller\Delivery;
+
+use App\Command\SendDeliveryCommand;
+use App\UI\Rest\Controller\CommandController;
+use App\UI\Rest\Response\ApiResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use function json_decode;
+
+/**
+ * @Route("/deliveries")
+ */
+class DeliveryCommandController extends CommandController
+{
+    public function __construct(MessageBusInterface $bus)
+    {
+        parent::__construct($bus);
+    }
+
+    /**
+     * @Route("/deliveries", methods={"POST"})
+     */
+    public function sendDelivery(Request $request): Response
+    {
+        $data = json_decode($request->getContent(), true);
+        $destination = $request->query->get('destination');
+
+        $command = new SendDeliveryCommand($data['category'], $destination);
+
+        $response = new ApiResponse();
+        return $response->setStatusCode(200);
+    }
+}
---
+++ src/UI/Rest/Controller/Delivery/DeliveryQueryController.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\UI\Rest\Controller\Delivery;
+
+use App\Query\CheckDeliveryStatusQuery;
+use App\UI\Rest\Controller\QueryController;
+use JsonApiPhp\JsonApi\Attribute;
+use JsonApiPhp\JsonApi\DataDocument;
+use JsonApiPhp\JsonApi\Link\SelfLink;
+use JsonApiPhp\JsonApi\ResourceObject;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+/**
+ * @Route("/deliveries")
+ */
+class DeliveryQueryController extends QueryController
+{
+    public function __construct(MessageBusInterface $queryBus)
+    {
+        parent::__construct($queryBus);
+    }
+
+    /**
+     * @Route("/{id}}")
+     */
+    public function checkStatus(Request $request, int $id): string
+    {
+        $destination = $request->query->get('destination');
+        $response = $this->ask(new CheckDeliveryStatusQuery($id, $destination));
+
+        return json_encode(new DataDocument(
+                new ResourceObject(
+                    'deliverie',
+                    (string)$id,
+                    new Attribute('delivery', $response),
+                    new SelfLink(sprintf('/deliveries/%d', $id))
+                )
+            )
+        );
+    }
+}
