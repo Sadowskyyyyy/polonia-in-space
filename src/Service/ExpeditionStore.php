e0b5c548271a44fab8dc819513e92879461f8c66
wiktorsadowski <wiktor.sadowski@landingi.com>
2021-08-24 10:57:43 +0200

    Add repository and domain expedition model.
    Added those classes to make operations and store model in database.

---
+++ src/Service/ExpeditionStore.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\DomainModel\Expedition;
+use App\Entity\ExpeditionEntity;
+use App\Shared\Domain\Exception\NotFoundException;
+use Doctrine\ORM\EntityManagerInterface;
+
+final class ExpeditionStore implements ExpeditionRepositoryInterface
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function save(Expedition $expedition): void
+    {
+        $this->entityManager->persist($expedition);
+        $this->entityManager->flush();
+    }
+
+    public function getById(int $id): Expedition
+    {
+        $entity = $this->entityManager->getRepository(ExpeditionEntity::class)->find($id);
+
+        if (empty($entity) === true) {
+            throw new NotFoundException();
+        }
+
+        return ExpeditionEntity::toDomain($entity);
+    }
+}
