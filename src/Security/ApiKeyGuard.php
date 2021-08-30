e4b19efebe2153bb789d8be8b6de66b1f81b65b0
wiktorsadowski <wiktor.sadowski@landingi.com>
2021-08-30 09:42:58 +0200

    Add authentication guard to check creditentials via apikey.

---
+++ src/Security/ApiKeyGuard.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Security;
+
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+
+class ApiKeyGuard extends AbstractGuardAuthenticator
+{
+    private UserRepository $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function start(Request $request, AuthenticationException $authException = null): JsonResponse
+    {
+        $data = [
+            'message' => 'Authentication Required'
+        ];
+
+        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function supports(Request $request): bool
+    {
+        return $request->headers->has('X-AUTH-TOKEN');
+    }
+
+    public function getCredentials(Request $request): mixed
+    {
+        return $request->headers->get('X-AUTH-TOKEN');
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider): ?UserInterface
+    {
+        if (empty($credentials) === true) {
+            return null;
+        }
+
+        $user = $this->userRepository->findOneBy(['apikey' => $credentials]);
+
+        if (empty($user) === true) {
+            throw new BadCredentialsException();
+        }
+
+        return $user;
+    }
+
+    public function checkCredentials($credentials, UserInterface $user): bool
+    {
+        return true;
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): JsonResponse
+    {
+        return new JsonResponse(Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        return null;
+    }
+
+    public function supportsRememberMe(): bool
+    {
+        return false;
+    }
+}
