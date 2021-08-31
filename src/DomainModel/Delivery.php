1dc674cad14e596127eced4e51f9e6d69d647bee
wiktorsadowski <wiktor.sadowski@landingi.com>
2021-08-31 11:54:36 +0200

    Some fixes and test :D

---
+++ src/DomainModel/Delivery.php
@@ -0,0 +1,87 @@
+<?php
+declare(strict_types=1);
+
+namespace App\DomainModel;
+
+use App\Exception\CannotChangeStatusOfDeliveredDeliveryException;
+
+class Delivery
+{
+    private AbstractScientist $sender;
+    private Product $product;
+    private string $destination;
+    private string $status;
+    private \DateTimeInterface $postDate;
+    private \DateTimeInterface $pickUpDate;
+
+    public function __construct(AbstractScientist $sender,
+                                Product           $product,
+                                string            $destination,
+                                string            $status)
+    {
+        $this->sender = $sender;
+        $this->product = $product;
+        $this->destination = $destination;
+        $this->status = $status;
+        $this->postDate = new \DateTime(\DateTimeInterface::RFC822);
+        $this->pickUpDate = clone $this->postDate;
+        $this->pickUpDate->modify('+14 minutes');
+    }
+
+    public static function createNewDelivery(
+        AbstractScientist $sender,
+        Product           $product,
+        string            $destination,
+        string            $status): Delivery
+    {
+        $now = new \DateTime();
+        $now = $now->getTimestamp();
+        $pickupDate = $now + (14 * 60 * 1000);
+
+        return new Delivery($sender, $product, $destination, $status, $now, $pickupDate);
+    }
+
+    public function changeStatusToDelivered(): void
+    {
+        $this->status = 'delivered';
+    }
+
+    public function changeStatusToSent(): void
+    {
+        if ('delivered' === $this->status) {
+            throw new CannotChangeStatusOfDeliveredDeliveryException();
+        }
+
+        $this->status = 'sent';
+    }
+
+    public function getSender(): AbstractScientist
+    {
+        return $this->sender;
+    }
+
+    public function getProduct(): Product
+    {
+        return $this->product;
+    }
+
+    public function getDestination(): string
+    {
+        return $this->destination;
+    }
+
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    public function getPostDate(): \DateTime
+    {
+        return $this->postDate;
+    }
+
+    public function getPickUpDate(): \DateTime
+    {
+        return $this->pickUpDate;
+    }
+}
---
+++ tests/Delivery/Domain/DeliveryTest.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Tests\Delivery\Domain;
+
+use App\DomainModel\Delivery;
+use App\DomainModel\EarthScientistDomain;
+use App\DomainModel\Product;
+use App\Exception\CannotChangeStatusOfDeliveredDeliveryException;
+use PHPUnit\Framework\TestCase;
+
+class DeliveryTest extends TestCase
+{
+    private Delivery $delivery;
+
+    /** @test */
+    public function test_should_change_delivery_status_to_sent_should_run_successful()
+    {
+        $this->delivery = Delivery::createNewDelivery(
+            new EarthScientistDomain(1, 'Adam', 'Jensen', 'pass'),
+            new Product('food'),
+        'spacestation',
+        '');
+
+        $this->delivery->changeStatusToSent();
+        $this->assertEquals('sent', $this->delivery->getStatus());
+    }
+
+    /** @test */
+    public function test_change_delivery_status_to_sent_should_throw_error()
+    {
+        $this->expectException(CannotChangeStatusOfDeliveredDeliveryException::class);
+        $this->delivery = Delivery::createNewDelivery(
+            new EarthScientistDomain(1, 'Adam', 'Jensen', 'pass'),
+            new Product('food'),
+            'spacestation',
+            'delivered');
+
+        $this->delivery->changeStatusToSent();
+    }
+}
