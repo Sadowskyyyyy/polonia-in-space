f5413fd23ae80ab08ad95269f890d7485f93b96f
wiktorsadowski <wiktor.sadowski@landingi.com>
2021-09-06 18:58:17 +0200

    Added acctual used repositories to infrastructure layer.

--- src/Service/AuthFactory.php
+++ src/Service/AuthFactory.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
-namespace App\Service;
 
+namespace App\Service;
 
 use App\DomainModel\Repository\EarthScientistRepository;
 use App\DomainModel\Repository\MarsScientistRepository;
